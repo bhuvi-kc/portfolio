f7a42a9c286c3bc90572137894b6ac91873b0901
bhuvi-kc <bhuvikumar1249@gmail.com>
2026-07-19 02:15:32 +0530

    final changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,9 @@ import projs3 from "../assets/portfolio-slide.png";
 import projj from "../assets/journey-slide.png";
 import projc from "../assets/contact-slide.png";
 import githubNote from "../assets/git-link.png";
+import bvpLogo from "../assets/bvp.png";
+import sggsLogo from "../assets/sggs.png";
+import tietLogo from "../assets/tiet.png";
 import "../App.css";
 
 const REPOS = {
@@ -18,7 +21,7 @@ const REPOS = {
 const SOCIALS = {
   github: "https://github.com/bhuvi-kc",
   linkedin: "https://www.linkedin.com/in/bhuvi0149/",
-  instagram: "https://www.instagram.com/?hl=en",
+  instagram: "https://www.instagram.com/ft_bhxviii/?hl=en",
 };
 
 function About() {
@@ -80,9 +83,49 @@ function About() {
         </div>
       </section>
 
-      <section className="section-slide">
-        <img src={projj} className="proj-j" />
-      </section>
+        <section className="section-slide">
+        <div className="proj-frame">
+            <img src={projj} className="proj-j" />
+
+            <div className="edu-box box-1">
+            <img src={bvpLogo} className="edu-logo" />
+            <div className="edu-text">
+                <h3>Bhavan Vidyalaya, Panchkula</h3>
+                <span className="strip-years">2019 – 2023</span>
+                <p>
+                Completed my schooling at Bhavan Vidyalaya, where I built a
+                strong academic foundation and grew my interest in technology
+                and coding. I also completed my class 10th from here.
+                </p>
+            </div>
+            </div>
+
+            <div className="edu-box box-2">
+            <img src={sggsLogo} className="edu-logo" />
+            <div className="edu-text">
+                <h3>Sri Guru Gobind Singh Public School, Chandigarh</h3>
+                <span className="strip-years">2024 – 2025</span>
+                <p>
+                Studied at SGGS school, exploring myself and expanding my
+                niche. I also completed my class 12th from here.
+                </p>
+            </div>
+            </div>
+
+            <div className="edu-box box-3">
+            <img src={tietLogo} className="edu-logo" />
+            <div className="edu-text">
+                <h3>Thapar Institute of Engineering and Technology</h3>
+                <span className="strip-years">2025 – 29</span>
+                <p>
+                Continuing my academic journey at TIET, focusing on
+                programming, engineering fundamentals, and most importantly,
+                finding myself and growing my interest in the binary world.
+                </p>
+            </div>
+            </div>
+        </div>
+        </section>
 
       <section className="section-slide">
         <div className="proj-frame">
